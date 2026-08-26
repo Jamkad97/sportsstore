8b34a5fa865525cc37f2ff28a15347993fcba727
Jamison Kadlec <Jamisonmkadlec@gmail.com>
2026-08-25 21:15:45 -0500

    handlebars

--- dist/routes/catalog.js
+++ dist/routes/catalog.js
@@ -3,7 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCatalogRoutes = void 0;
 const createCatalogRoutes = (app) => {
     app.get("/", (req, resp) => {
-        resp.send("Hello, SportsStore Route");
+        resp.render("index");
+    });
+    app.get("/err", (req, resp) => {
+        throw new Error("Something bad happened");
+    });
+    app.get("/asyncerr", async (req, resp) => {
+        throw new Error("Something bad happened asynchronously");
     });
 };
 exports.createCatalogRoutes = createCatalogRoutes;
--- src/routes/catalog.ts
+++ src/routes/catalog.ts
@@ -1,6 +1,13 @@
 import { Express } from "express";
 export const createCatalogRoutes = (app: Express) => {
     app.get("/", (req, resp) => {
-        resp.send("Hello, SportsStore Route");
+        resp.render("index");
     })
+    app.get("/err", (req, resp) => {
+        throw new Error ("Something bad happened");
+    });
+   
+    app.get("/asyncerr", async (req, resp) => {
+        throw new Error ("Something bad happened asynchronously");
+    });
 }
