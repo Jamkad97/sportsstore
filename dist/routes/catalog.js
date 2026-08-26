af4d66a94b81ec0ad6c8ea06ff84270c2ab6157c
Jamison Kadlec <Jamisonmkadlec@gmail.com>
2026-08-25 21:46:25 -0500

    Sequelized

--- dist/routes/catalog.js
+++ dist/routes/catalog.js
@@ -1,15 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCatalogRoutes = void 0;
+const data_1 = require("../data");
 const createCatalogRoutes = (app) => {
-    app.get("/", (req, resp) => {
-        resp.render("index");
-    });
-    app.get("/err", (req, resp) => {
-        throw new Error("Something bad happened");
-    });
-    app.get("/asyncerr", async (req, resp) => {
-        throw new Error("Something bad happened asynchronously");
+    app.get("/", async (req, resp) => {
+        const products = await data_1.catalog_repository.getProducts();
+        resp.render("index", { products });
     });
 };
 exports.createCatalogRoutes = createCatalogRoutes;
