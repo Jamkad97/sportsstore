7e8831db3239b5e75ab6b1e68e46cb96c02af8c1
Jamison Kadlec <Jamisonmkadlec@gmail.com>
2026-09-21 11:40:37 -0500

    Week 3&4 commit 4

--- src/data/orm/queries.ts
+++ src/data/orm/queries.ts
@@ -38,6 +38,11 @@ export function AddQueries<TBase extends Constructor<BaseRepo>>(Base: TBase) {
    
         getSuppliers() {
             return SupplierModel.findAll({raw: true, nest: true});
-        }       
+        }
+        getProductDetails(ids: number[]) {
+            return ProductModel.findAll({
+                where: { id: { [Op.in]: ids }}, raw: true, nest: true,
+            });
+        }    
     }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Express } from "express";
 import { createCatalogRoutes } from "./catalog";
-
+import { createCartMiddleware, createCartRoutes } from "./cart";
 export const createRoutes = (app: Express) => {
-
+    createCartMiddleware(app);
     createCatalogRoutes(app);
+    createCartRoutes(app);
 }
